2f36a2b8db767bd20c5a804cc18682ffd43fa153
dpraja <raja.pushpanathan@gmail.com>
2018-09-29 16:40:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
-
+@app.route('/', methods=['GET'])
+def banuhook():
+    return("welcome thalaivi banu")
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
