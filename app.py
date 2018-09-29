db213c8ce953ab8dada04196fb082473580fa758
dpraja <raja.pushpanathan@gmail.com>
2018-09-29 16:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("action") != "bookappt":    
-        return {}
+    #if req.get("action") != "bookappt":    
+    #    return {}
     
     result = req.get("result")
     parameters = result.get("parameters")
